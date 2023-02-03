8da79a85fc5f0e89e0f855af9567713efbb149ca
D3BUG-RED <121511135+D3BUG-RED@users.noreply.github.com>
2023-02-04 00:25:48 +0300

    UI

---
+++ RootGameJam2D/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+    
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString();
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(3);
+        confirmationPrompt.SetActive(false);
+    }
+
+    
+
+}
