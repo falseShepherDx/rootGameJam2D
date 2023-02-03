c35b448e823b322195667495a77231dc6ae3164d
Emin Gürzoğlu <gurzoglum@gmail.com>
2023-02-04 02:51:40 +0300

    EnemyAIUpdate

---
+++ RootGameJam2D/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+
+    void Awake()
+    {
+        playerPos = GameObject.Find("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
+        {
+            transform.position=Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);  
+        }   
+        
+    }
+}
