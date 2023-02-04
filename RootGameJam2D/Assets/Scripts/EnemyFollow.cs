295ff71e1fde21aa9aca43de6b78dd2ccc1cb71a
Emin Gürzoğlu <gurzoglum@gmail.com>
2023-02-05 02:47:37 +0300

    ..

--- RootGameJam2D/Assets/Scripts/EnemyFollow.cs
+++ RootGameJam2D/Assets/Scripts/EnemyFollow.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    private List<Rigidbody2D> EnemyRBs; 
+    private List<Rigidbody2D> EnemyRBs;
     public float speed;
     private Transform playerPos;
     private float repelRange = 0.5f;
     private Rigidbody2D rb;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,22 +18,27 @@ public class EnemyFollow : MonoBehaviour
         {
             EnemyRBs = new List<Rigidbody2D>();
         }
-        EnemyRBs.Add(rb);   
-
+        EnemyRBs.Add(rb);
     }
+
     private void OnDestroy()
     {
-        EnemyRBs.Remove(rb); 
+        EnemyRBs.Remove(rb);
     }
 
-    // Update is called once per frame
     void Update()
-    {if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
+    {
+        if (Vector2.Distance(transform.position, playerPos.position) > 0.25f)
         {
-            transform.position=Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);  
-        }   
-        
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+
+            Vector2 direction = (playerPos.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            angle += 180;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
+
     private void FixedUpdate()
     {
         Vector2 repelForce = Vector2.zero;
@@ -42,12 +48,11 @@ public class EnemyFollow : MonoBehaviour
             {
                 continue;
             }
-            if(Vector2.Distance(enemy.position, rb.position) <= repelRange)
+            if (Vector2.Distance(enemy.position, rb.position) <= repelRange)
             {
                 Vector2 repelDir = (rb.position - enemy.position).normalized;
                 repelForce += repelDir;
             }
         }
-        
     }
 }
--- RootGameJam2D/Assets/Scripts/EnemyHealth.cs
+++ RootGameJam2D/Assets/Scripts/EnemyHealth.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] float health;
-   
+    [SerializeField] public int health;
+    GainExperience xP;
+
+    private void Start()
+    {
+        xP = GameObject.Find("Player").GetComponent<GainExperience>();
+    }
+
+
     void Update()
     {
-        if (health <=0f)
+        if (health <= 0)
         {
             Destroy(gameObject);
+            xP.exp();
         }
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
